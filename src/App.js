8abde9bfa6ef84feba1c87da1bf1c20e700adb0e
Garrett <garrettojones@gmail.com>
2021-09-10 09:43:56 -0500

    removed css files. too many issues and not enough time today

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import './App.css'
+
 
 const App = () => {
     const [data, setData] = useState([]);
-
     useEffect(() => {
         axios.get(
         'https://www.reddit.com/r/programmerhumor/top.json',
